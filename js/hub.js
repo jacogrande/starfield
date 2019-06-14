7fbe0e3a53de9c370850ef8bb86bf2c7b81d7045
jacogrande <prowelljackson@gmail.com>
2019-06-13 20:08:46 -0700

    Added hub toggling

--- js/hub.js
+++ js/hub.js
@@ -1,18 +1,50 @@
-let hub = null;
+const refreshHubData = () => {
+  return `
+    <br>
+    <p>Fuel: ${Ship.fuel.toString().substring(0, 7)} </p>
+
+  `;
+}
+
 const createHub = () => {
-  hub = createLog("hub", `
+  let hub = createLog("hub", `
     position:absolute;
     top:0% !important;
     left:0% !important;
     width:25%;
-    height:96%;
+    height:48%;
     background:rgba(100,100,100,0.2);
-    color:white;
+    color:rgba(255,255,255,0.8);
     text-align:center;
   `);
 
-  hub.append("<br>h &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; u &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; b");
+  const hubHeader = `
+    <h2>h &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; u &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; b</h2>
+    <p style='color:rgba(255,255,255,0.5)'> Press <span style='color:rgba(116, 201, 255,0.5)'>'t'</span> to toggle </p>
+  `;
+  let hubData = refreshHubData();
+
+  hub.append(hubHeader);
+  hub.append(hubData);
+
+  // setup hub toggling
+  hub.isToggled = false;
+  hub.toggle = () => {
+    // toggle the hub
+    hub.isToggled = !hub.isToggled;
+    // if the hub is now toggled, make it invisible
+    hub.isToggled ? hub.style.visibility = "hidden" : hub.style.visibility = "visible";
+    hub.isToggled ? fpsLog.style.visibility = "hidden" : fpsLog.style.visibility = "visible";
+
+  }
+
+  hub.updateData = ()=>{
+    hubData = refreshHubData()
+    hub.write(hubHeader+hubData);
+  }
+
+  return hub;
 
 }
 
-createHub();
+const hub = createHub();
---
+++ js/setup.js
@@ -0,0 +1,8 @@
+// setup canvas
+let canvas = createCanvas('canvas1', 'default', 'background:#000');
+canvas.fillStyle='rgb(255,255,255)';
+
+// start fps log
+let fpsLog = createLog('fpsLog');
+
+let Fps = 60;
